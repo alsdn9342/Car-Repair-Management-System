ac6abbfbeacf491c5a3e69d4bf67a87691eee270
alsdn9342 <65743649+alsdn9342@users.noreply.github.com>
2021-07-15 12:26:14 +0900

    Project init

---
+++ DataAccess.cs
@@ -0,0 +1,454 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace Assignment2_MinWooPark
+{  
+    class DataAccess
+    {
+        private SqlConnection _conn;
+        private SqlDataAdapter _adapter;
+        private SqlCommandBuilder _cmdBuilder;
+        private DataSet _dataSet;
+        private DataTable _tblVehicle; 
+        private DataTable _tblInventory;
+        private DataTable _tblRepair;
+
+        // constructor
+        public DataAccess()
+        {
+            string cs = GetConnectionString("CarRepairManagementSystemMdf");   // change the connection string name to match yours
+            string query = "Select ID, make, model, year, newOrUsed from Vehicle; Select ID, vehicleID, numberOnhand, price, cost from Inventory; Select ID, inventoryID,whatTorepair from Repair";
+     
+            _conn = new SqlConnection(cs);
+            _adapter = new SqlDataAdapter(query, _conn);
+            _cmdBuilder = new SqlCommandBuilder(_adapter);
+        
+            FillDataSet();
+        }
+
+        // method to read the connection string from the JSON file
+        public string GetConnectionString(string connectionStringName)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile("config.json");
+            IConfiguration config = configurationBuilder.Build();
+
+            return config["ConnectionStrings:" + connectionStringName];
+        }
+
+        // method to refresh the dataset
+        private void FillDataSet()
+        {
+            _conn.Close();
+            // reset the dataset
+            _dataSet = new DataSet();
+
+            _adapter.Fill(_dataSet);
+
+            _tblVehicle = _dataSet.Tables[0];
+            _tblInventory = _dataSet.Tables[1];
+            _tblRepair = _dataSet.Tables[2];
+
+            // define primary key for vehicle
+            DataColumn[] pk = new DataColumn[1];
+            pk[0] = _tblVehicle.Columns["ID"];
+            _tblVehicle.PrimaryKey = pk;
+
+            // define primary key for inventory
+            DataColumn[] pk1 = new DataColumn[1];
+            pk1[0] = _tblInventory.Columns["ID"];
+            _tblInventory.PrimaryKey = pk1;
+
+            //define primary key for repair
+            DataColumn[] pk2 = new DataColumn[1];
+            pk2[0] = _tblRepair.Columns["ID"];
+            _tblRepair.PrimaryKey = pk2;
+
+            DataColumn parentInVehicle;
+            DataColumn childrenInInventory;
+            parentInVehicle = _dataSet.Tables[0].Columns["ID"];
+            childrenInInventory = _dataSet.Tables[1].Columns["vehicleID"];
+
+            ForeignKeyConstraint inventoryFK = new ForeignKeyConstraint(parentInVehicle,childrenInInventory);
+            inventoryFK.DeleteRule = Rule.Cascade;
+            // Cannot delete a customer value that has associated existing orders.  
+            _dataSet.Tables[1].Constraints.Add(inventoryFK);
+            _dataSet.EnforceConstraints = true;
+
+            DataColumn parentInInventory;
+            DataColumn childrenInRepair;
+            parentInInventory = _dataSet.Tables[1].Columns["ID"];
+            childrenInRepair = _dataSet.Tables[2].Columns["inventoryID"];
+            ForeignKeyConstraint repairFK = new ForeignKeyConstraint(parentInInventory, childrenInRepair);
+            repairFK.DeleteRule = Rule.Cascade;
+            _dataSet.Tables[2].Constraints.Add(repairFK);
+            _dataSet.EnforceConstraints = true;
+        }
+
+        public void GetAllVehicles()
+        {
+            // display products
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine($"{"ID",5} {"make",10} {"model",10} {"year",10} {"newOrUsed",10}");
+            Console.WriteLine();
+            foreach (DataRow row in _tblVehicle.Rows)
+            {
+                Console.WriteLine($"{row["ID"],5} {row["make"],10} {row["model"],10} {row["year"],10} {row["newOrUsed"],10}");
+            }
+        }
+
+        public void GetAllInventory()
+        {
+            // display products
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine($"{"ID",5} {"vehicleID",10} {"numberOnhand",10} {"price",10} {"cost",10}");
+            Console.WriteLine();
+            foreach (DataRow row in _tblInventory.Rows)
+            {
+                Console.WriteLine($"{row["ID"],5} {row["vehicleID"],10} {row["numberOnhand"],10} {row["price"],10} {row["cost"],10}");
+            }
+        }
+
+        public void GetAllRepair()
+        {
+            // display products
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine($"{"ID",10} {"inventoryID",10} {"whatTorepair",10}" );
+            Console.WriteLine();
+            foreach (DataRow row in _tblRepair.Rows)
+            {
+                Console.WriteLine($"{row["ID"],10} {row["inventoryID"],10} {row["whatTorepair"],10}");
+            }
+        }
+
+        public void InsertVehicle(string make, string model, int year, string newOrUsed)
+        {
+            DataRow newRow = _tblVehicle.NewRow();  // create a new row
+            newRow["ID"] = 0;                 // add a dummy value to ProductID, 
+                                                     // since it'll be overwritten by the SQL Server 
+                                                   // because ProductID is set as 
+                                                     // Identity (Auto-increment) in the database
+            newRow["make"] = make;
+            newRow["model"] = model;
+            newRow["year"] = year;
+            newRow["newOrUsed"] = newOrUsed;
+            _tblVehicle.Rows.Add(newRow);           // add the new row to the Rows collection
+
+            // read the INSERT query from the SqlCommandBuilder object
+            _adapter.InsertCommand = _cmdBuilder.GetInsertCommand();
+            _adapter.Update(_tblVehicle);  // save the changes to database
+
+            FillDataSet();  // refresh dataset
+        }
+
+        public void InsertInventory(int vehicleId, string numberOnhand,string price, string cost)
+        {
+            DataRow vehicleRow = _tblVehicle.Rows.Find(vehicleId);
+     
+            //check vehicle id exists or not
+            if (vehicleRow == null)
+            {
+                Console.WriteLine("you insert invalid vehicle Id. Try again.");
+            }
+            else
+            {
+                bool checkVehicleId = true;
+                
+                // check if vehicle id was used or not.
+                 foreach (DataRow row in _tblInventory.Rows)
+               {
+                    int vehicleID = int.Parse($"{row["vehicleID"],10}") ;
+
+                    if (vehicleID == vehicleId)
+                   {
+                        checkVehicleId = false;
+                   }
+               }
+
+                if(checkVehicleId == true)
+                {
+                   
+                    string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                    string query = "Insert into Inventory (vehicleID, numberOnhand, price, cost) values (@vehicleId, @numberOnhand, @price, @cost)";
+
+                    
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, _conn);
+                        cmd.Parameters.AddWithValue("vehicleID", vehicleId);
+                        cmd.Parameters.AddWithValue("numberOnhand", numberOnhand);
+                        cmd.Parameters.AddWithValue("price", price);
+                        cmd.Parameters.AddWithValue("cost", cost);
+                        _conn.Open();
+
+                        int result = cmd.ExecuteNonQuery();
+
+                        if(result ==1)
+                        {
+                            Console.WriteLine("It successfuly is inserted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not Inserted");
+                        }
+                    }
+
+                    FillDataSet();  // refresh dataset
+                } else
+                {
+                    Console.WriteLine(vehicleId + " already was used. Try differnet vehicle ID ");
+                }
+            }
+        }
+
+        public void InsertRepair(int inventoryId, string whatToRepair)
+        {
+            DataRow inventoryRow = _tblInventory.Rows.Find(inventoryId);
+
+            //check vehicle id exists or not
+            if (inventoryRow == null)
+            {
+                Console.WriteLine("you insert invalid vehicle Id. Try again.");
+            }
+            else
+            {
+                bool checkInventoryId = true;
+
+                // check if vehicle id was used or not.
+                foreach (DataRow row in _tblRepair.Rows)
+                {
+                    int inventoryID = int.Parse($"{row["inventoryID"],10}");
+
+                    if (inventoryID == inventoryId)
+                    {
+                        checkInventoryId = false;
+                    }
+                }
+
+                if (checkInventoryId == true)
+                {
+
+                    string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                    string query = "Insert into Repair (inventoryID, whatTorepair) values (@inventoryId, @whatToRepair)";
+
+
+                    using (SqlConnection conn = new SqlConnection(cs))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, _conn);
+                        cmd.Parameters.AddWithValue("inventoryID", inventoryId);
+                        cmd.Parameters.AddWithValue("whatTorepair", whatToRepair);
+                        
+                        _conn.Open();
+
+                        int result = cmd.ExecuteNonQuery();
+
+                        if (result == 1)
+                        {
+                            Console.WriteLine("It successfuly is inserted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not Inserted");
+                        }
+                    }
+
+                    FillDataSet();  // refresh dataset
+                }
+                else
+                {
+                    Console.WriteLine("Inventory ID " + inventoryId + " is already was used. Try differnet inventory ID ");
+                }
+            }
+        }
+
+        public void UpdateVehicle(int id, string make, string model, int year, string newOrUsed)
+        {
+            DataRow row = _tblVehicle.Rows.Find(id);
+
+            if (row != null)
+            {
+                row["make"] = make;
+                row["model"] = model;
+                row["year"] = year;
+                row["newOrUsed"] = newOrUsed;
+
+                _adapter.UpdateCommand = _cmdBuilder.GetUpdateCommand();
+                _adapter.Update(_tblVehicle);
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid vehicle ID. Please try again.");
+        }
+
+        public void UpdateInventory(int id, string numberOnhand, int price, int cost)
+        {
+            DataRow row = _tblInventory.Rows.Find(id);
+
+            if (row != null)
+            {
+
+                string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                string query = "Update Inventory SET numberOnhand = @numberOnhand, price = @price, cost= @cost where ID = @id";
+
+
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, _conn);
+
+                    cmd.Parameters.AddWithValue("ID", id);
+                    cmd.Parameters.AddWithValue("numberOnhand", numberOnhand);
+                    cmd.Parameters.AddWithValue("price", price);
+                    cmd.Parameters.AddWithValue("cost", cost);
+                    _conn.Open();
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result == 1)
+                    {
+                        Console.WriteLine("It successfuly is modified");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Modified");
+                    }
+                }
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid Inventory ID. Please try again.");
+        }
+
+        public void UpdateRepair(int id, string whatToRepair)
+        {
+            DataRow row = _tblRepair.Rows.Find(id);
+
+            if (row != null)
+            {
+
+                string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                string query = "Update Repair SET whatTorepair = @whatToRepair where ID = @id";
+
+
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, _conn);
+
+                    cmd.Parameters.AddWithValue("ID", id);
+                    cmd.Parameters.AddWithValue("whatTorepair", whatToRepair);
+                   
+                    _conn.Open();
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result == 1)
+                    {
+                        Console.WriteLine("It successfuly is modified");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Modified");
+                    }
+                }
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid Repair ID. Please try again.");
+        }
+
+        public void DeleteVehicle(int id)
+        {
+            DataRow row = _tblVehicle.Rows.Find(id);
+
+            if (row != null)
+            {
+                row.Delete();
+
+                _adapter.DeleteCommand = _cmdBuilder.GetDeleteCommand();
+                _adapter.Update(_tblVehicle);
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid Vehicle ID. Please try again.");
+        }
+
+        public void DeleteInventory(int id)
+        {
+            DataRow row = _tblInventory.Rows.Find(id);
+
+            if (row != null)
+            {
+                string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                string query = "Delete from Inventory where ID = @id";
+
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, _conn);
+
+                    cmd.Parameters.AddWithValue("ID", id);
+                    _conn.Open();
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result == 1)
+                    {
+                        Console.WriteLine("It successfuly is deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Deleted");
+                    }
+                }
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid Inventory ID. Please try again.");
+        }
+
+        public void DeleteRepair(int id)
+        {
+            DataRow row = _tblRepair.Rows.Find(id);
+
+            if (row != null)
+            {
+                string cs = GetConnectionString("CarRepairManagementSystemMdf");
+                string query = "Delete from Repair where ID = @id";
+
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand(query, _conn);
+
+                    cmd.Parameters.AddWithValue("ID", id);
+                    _conn.Open();
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result == 1)
+                    {
+                        Console.WriteLine("It successfuly is deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Deleted");
+                    }
+                }
+
+                FillDataSet();
+            }
+            else
+                Console.WriteLine("\nInvalid Repair ID. Please try again.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+
+namespace Assignment2_MinWooPark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess crud = new DataAccess();
+            Boolean menu = true;
+
+            do
+            {
+                int choice = DisplayMenu();
+
+                    if (choice == 1)
+                    {
+
+                    bool vehicle = true;
+
+                    while (vehicle)
+                    {
+                        int choiceOfVehicle = DisplayVehicleMenu();
+
+                        switch (choiceOfVehicle)
+                        {
+                            case 1: // Get All vehicles
+                                crud.GetAllVehicles();
+                                break;
+
+
+                            case 2: // Insert new vehicle
+                                Console.Write("\nEnter make: ");
+                                string make = Console.ReadLine();
+
+                                Console.Write("Enter model: ");
+                                string model = Console.ReadLine();
+
+                                Console.Write("Enter year: ");
+                                int year = int.Parse(Console.ReadLine());
+
+                                Console.Write("Enter new or used: ");
+                                string newOrUsed = Console.ReadLine();
+
+                                crud.InsertVehicle(make, model, year, newOrUsed);
+                                break;
+
+                            case 3: // update Product
+                                Console.Write("\nEnter ID: ");
+                                int id = int.Parse(Console.ReadLine());
+
+                                Console.Write("\nEnter make: ");
+                                string makeUpdate = Console.ReadLine();
+
+                                Console.Write("Enter model: ");
+                                string modelUpdate = Console.ReadLine();
+
+                                Console.Write("Enter year: ");
+                                int yearUpdate = int.Parse(Console.ReadLine());
+
+                                Console.Write("Enter new or used: ");
+                                string newOrUsedUpdate = Console.ReadLine();
+
+
+                                crud.UpdateVehicle(id, makeUpdate, modelUpdate, yearUpdate, newOrUsedUpdate);
+                                break;
+
+
+                            case 4: // Delete vehicle
+                                Console.Write("\nEnter Product Id: ");
+                                id = int.Parse(Console.ReadLine());
+
+                                crud.DeleteVehicle(id);
+                                break;
+
+                            case 5:
+                                vehicle = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Invalid option! Try again!");
+                                break;
+                        }
+                    }
+
+                 
+                    }
+                    else if (choice == 2)
+                    {
+                    bool inventory = true;
+
+                    while (inventory)
+                    {
+                        int choiceOfInventory = DisplayInventoryMenu();
+
+                        switch (choiceOfInventory)
+                        {
+                              case 1: // Insert new vehicle
+                                   Console.Write("\nEnter vehicle ID: ");
+                                   int vehicleID = int.Parse(Console.ReadLine()); 
+
+                                   Console.Write("Enter number on hand: ");
+                                   string numbernhand = Console.ReadLine();
+
+                                   Console.Write("Enter price: ");
+                                   string price = Console.ReadLine();
+
+                                   Console.Write("Enter cost: ");
+                                   string cost = Console.ReadLine();
+
+                                   crud.InsertInventory(vehicleID, numbernhand, price, cost);
+                                   break;
+
+                              case 2: // Get All vehicles
+                                   crud.GetAllInventory();
+                                   break;
+                            
+                              case 3: 
+                                     Console.Write("\nEnter Product Id: ");
+                                     int id = int.Parse(Console.ReadLine());
+
+                                     Console.Write("Enter number on hand: ");
+                                     string numbernhandToUpdate = Console.ReadLine();
+
+                                      Console.Write("Enter price: ");
+                                      int priceToUpdate = int.Parse(Console.ReadLine());
+
+                                      Console.Write("Enter cost: ");
+                                      int costToUpdate = int.Parse(Console.ReadLine());
+                                      
+                                      crud.UpdateInventory(id,numbernhandToUpdate,priceToUpdate,costToUpdate);
+                                      break;
+                              case 4:
+                                      Console.Write("\nEnter Product Id: ");
+                                      int idToDelete = int.Parse(Console.ReadLine());
+
+                                      crud.DeleteInventory(idToDelete);
+                                      break;
+
+
+                              case 5:
+                                     inventory = false;
+                                     break;
+
+                             default:
+                                    Console.WriteLine("Invalid option! Try again!");
+                                    break;
+                        
+                        }
+                    }
+
+
+                    }
+                    else if (choice == 3)
+                    {
+
+                    bool repair = true;
+
+                    while (repair)
+                    {
+                        int choiceOfRepair = DisplayRepairMenu();
+
+                        switch (choiceOfRepair)
+                        {
+                            
+                            case 1: // Insert new repair
+                                Console.Write("\nEnter inventory ID: ");
+                                int inventoryID = int.Parse(Console.ReadLine());
+
+                                Console.Write("Enter what to repair: ");
+                                string whatToRepair = Console.ReadLine();
+
+                                crud.InsertRepair(inventoryID,whatToRepair);
+                                break;
+                          
+
+                            case 2:
+                                crud.GetAllRepair();
+                                break;
+                            
+                               case 3:
+                                   Console.Write("\nEnter Repair Id: ");
+                                   int id = int.Parse(Console.ReadLine());
+
+                                   Console.Write("Enter what to repair: ");
+                                   string whatToRepairToUpdate = Console.ReadLine();
+
+                                   crud.UpdateRepair(id, whatToRepairToUpdate);
+                                   break;
+                            
+                            case 4:
+                                   Console.Write("\nEnter Product Id: ");
+                                   int idToDelete = int.Parse(Console.ReadLine());
+
+                                   crud.DeleteRepair(idToDelete);
+                                   break;
+
+                            case 5:
+                                repair = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Invalid option! Try again!");
+                                break;
+
+                        }
+                    }
+
+                    }
+                    else if (choice == 4)
+                    {
+                    menu = false;
+                    }
+                    
+
+                
+            } while (menu);
+        }
+
+        static int DisplayMenu()
+        {
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine("\t1 - Press 1 to modify vehicles");
+            Console.WriteLine("\t2 - Press 2 to modify inventory");
+            Console.WriteLine("\t3 - Press 3 to modify repair");
+            Console.WriteLine("\t4 - Press 4 to exit program");
+
+            Console.Write("\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int DisplayVehicleMenu()
+        {
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine("\t1 - Press 1 to list all vehicles");
+            Console.WriteLine("\t2 - Press 2 to add a new vehicle");
+            Console.WriteLine("\t3 - Press 3 to update vehicle");
+            Console.WriteLine("\t4 - Press 4 to delete program");
+            Console.WriteLine("\t5 - Press 5 to return to main menu");
+
+            Console.Write("\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int DisplayInventoryMenu()
+        {
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine("\t1 - Press 1 to insert new inventory");
+            Console.WriteLine("\t2 - Press 2 to view inventory for vehicle");
+            Console.WriteLine("\t3 - Press 3 to update inventory");
+            Console.WriteLine("\t4 - Press 4 to delete inventory");
+            Console.WriteLine("\t5 - Press 5 to return to main menu");
+
+            Console.Write("\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int DisplayRepairMenu()
+        {
+            Console.WriteLine("\n\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+            Console.WriteLine("\t1 - Press 1 to insert new repair");
+            Console.WriteLine("\t2 - Press 2 to view all repair");
+            Console.WriteLine("\t3 - Press 3 to update repair");
+            Console.WriteLine("\t4 - Press 4 to delete repair");
+            Console.WriteLine("\t5 - Press 5 to return to main menu");
+
+            Console.Write("\nEnter your choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
+    
+
